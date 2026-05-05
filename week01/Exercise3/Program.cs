663f0ef0f0bd160b13e87e95c68ddd94221d38e7
Ethanjames411 <ewalker@ethanjstudio.com>
2026-05-05 16:01:02 +0000

    week1 exercise2 first attempt

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        var random = new Random();
+        int MagicNumber = random.Next(1, 101);
+
+        int GuessNumber = -1;
+        do
+        {
+            Console.Write("what is your guess ");
+            GuessNumber = int.Parse(Console.ReadLine());
+
+            if (GuessNumber < MagicNumber)
+            {
+                Console.WriteLine("higher");
+            }
+
+            else if (GuessNumber > MagicNumber)
+            {
+                Console.WriteLine("lower");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
+        while (GuessNumber != MagicNumber);
     }
 }
