f1c36d6274687e27c60e9364b2dbee28324fabc5
Ethanjames411 <ewalker@ethanjstudio.com>
2026-05-06 21:23:15 +0000

    week1 exercise5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayMessage();
+
+        var userName = PromptUserName();
+        var userNumber = PromptUserNumber();
+        var valueSquared = SquareNumber(userNumber);
+
+        DisplayResulte(userName, valueSquared);
+    }
+
+    static void DisplayMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        var userName = Console.ReadLine();
+
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        var userNumberValue = Console.ReadLine();
+        int userNumber = int.Parse(userNumberValue);
+
+        return userNumber;
+    }
+
+    static int SquareNumber(int value)
+    {
+        var aquare = value * value;
+        return aquare;
+    }
+
+    static void DisplayResulte(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
